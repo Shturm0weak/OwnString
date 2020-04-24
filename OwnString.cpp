360c6159216c6a1737ab693bb3ce258a8b4207b0
Alexander <turlak01@mail.ru>
2020-04-25 00:38:13 +0300

    Third commit

--- OwnString.cpp
+++ OwnString.cpp
@@ -3,11 +3,7 @@
 #include <string>
 
 int main() {
-	while (true) {
-		std::string std("");
-		Custom::String string("Nice");
-		Custom::String string1(" play");
-		std::cin >> string1;
-		std::cout << string1 << std::endl;
-	}
+	Custom::String string("Nice");
+	Custom::String string1(" play");
+	std::cout << string + " team" + string1 << std::endl;
 }
